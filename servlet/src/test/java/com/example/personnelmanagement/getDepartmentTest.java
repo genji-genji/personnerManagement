0b8a75ebc463b419a9218b4cf7e4b31f5f7df751
husuyuan <864441605@qq.com>
2020-06-24 20:55:31 +0800

    StaffBean

---
+++ servlet/src/test/java/com/example/personnelmanagement/getDepartmentTest.java
@@ -0,0 +1,25 @@
+package com.example.personnelmanagement;
+
+import com.example.personnelmanagement.bean.StaffBean;
+import com.example.personnelmanagement.bean.UserBean;
+import com.example.personnelmanagement.service.StaffService;
+import com.example.personnelmanagement.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class getDepartmentTest {
+
+    @Autowired
+    StaffService staffService;
+
+    @Test
+    public void test() {
+        StaffBean staffBean = staffService.getDepartment(2);
+        System.out.println(staffBean);
+    }
+}
