4366dc53f30fc07ecad60901fe73e02a1c56a592
genji <a949403939@163.com>
2020-06-24 16:44:40 +0800

    StaffBean

---
+++ servlet/src/main/java/com/example/personnelmanagement/bean/messageBean.java
@@ -0,0 +1,11 @@
+package com.example.personnelmanagement.bean;
+
+public class messageBean {
+    private String result;
+    private String message;
+
+    public messageBean(String result,String message){
+        this.result=result;
+        this.message=message;
+    }
+}
